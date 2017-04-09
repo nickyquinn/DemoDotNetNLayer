45b56a7058422686d7abb32436f6a05c8b9245e5
Nick Quinn <nquinnathome1@gmail.com>
2017-04-09 12:24:06 +0100

    Updated validations on the DTO

--- DemoApp.Services/DTO/PersonDto.cs
+++ DemoApp.Services/DTO/PersonDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DemoApp.Services.DTO
@@ -7,9 +8,19 @@ namespace DemoApp.Services.DTO
     public class PersonDto
     {
         public int PersonDtoId { get; set; }
+        [Required]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Email address")]
+        [RegularExpression("^[A-Za-z0-9](([_\\.\\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\\.\\-]?[a-zA-Z0-9]+)*)\\.([A-Za-z]{2,})$", 
+            ErrorMessage= "You must enter a valid email address")]
         public string EmailAddress { get; set; }
+        [Required]
+        [Display(Name = "Age")]
         public int Age { get; set; }
     }
 }
